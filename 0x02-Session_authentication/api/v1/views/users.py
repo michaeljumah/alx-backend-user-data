fd050ad7563c624cf907def0b7dec41d435c91e7
michaeljumah <michaeljumah8@gmail.com>
2024-04-18 15:57:23 -0700

    users.py

--- 0x02-Session_authentication/api/v1/views/users.py
+++ 0x02-Session_authentication/api/v1/views/users.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 """ Module of Users views
 """
-from api.v1.views import app_views
 from flask import abort, jsonify, request
+
+from api.v1.views import app_views
 from models.user import User
 
 
@@ -25,6 +26,13 @@ def view_one_user(user_id: str = None) -> str:
       - User object JSON represented
       - 404 if the User ID doesn't exist
     """
+    if user_id is None:
+        abort(404)
+    if user_id == 'me':
+        if request.current_user is None:
+            abort(404)
+        else:
+            return jsonify(request.current_user.to_json())
     if user_id is None:
         abort(404)
     user = User.get(user_id)
