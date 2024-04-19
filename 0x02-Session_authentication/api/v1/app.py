118f00ec38e149d0e2832f44733762da32e09736
michaeljumah <michaeljumah8@gmail.com>
2024-04-18 17:56:59 -0700

    gj

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
-""" Route module for the API
 """
+Route module for the API
+"""
+from os import getenv
 from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 import os
-from os import getenv
 
 
 app = Flask(__name__)
@@ -13,7 +14,7 @@ app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = None
-AUTH_TYPE = getenv("AUTH_TYPE")
+AUTH_TYPE = getenv('AUTH_TYPE')
 
 if AUTH_TYPE == "auth":
     from api.v1.auth.auth import Auth
